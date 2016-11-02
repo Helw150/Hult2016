100590b01c7f35345adae01df0a12957420a912e
wbh230@nyu.edu <wbh230@nyu.edu>
2016-11-02 11:56:45 +0400

    New front-end of the app from scratch

---
+++ www/js/routes.js
@@ -0,0 +1,53 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('menu.login', {
+    url: '/login',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/login.html',
+        controller: 'loginCtrl'
+      }
+    }
+  })
+
+  .state('menu.main', {
+    url: '/main',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/main.html',
+        controller: 'mainCtrl'
+      }
+    }
+  })
+
+  .state('menu.profile', {
+    url: '/profile',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/profile.html',
+        controller: 'profileCtrl'
+      }
+    }
+  })
+
+  .state('menu', {
+    url: '/side-menu21',
+    templateUrl: 'templates/menu.html',
+    controller: 'menuCtrl'
+  })
+
+$urlRouterProvider.otherwise('/side-menu21/login')
+
+  
+
+});
